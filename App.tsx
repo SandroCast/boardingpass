1b163bdd13a988291fdf60c1912e5ba88775c103
sandro.castro <sandro.castro>
2024-09-17 14:51:52 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import {
+  useFonts,
+  Poppins_700Bold,
+  Poppins_500Medium,
+  Poppins_400Regular,
+  Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins"
+
+import { Home } from "@/app/home";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_700Bold,
+    Poppins_500Medium,
+    Poppins_400Regular,
+    Poppins_800ExtraBold
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return <Home />
+}
+
+
